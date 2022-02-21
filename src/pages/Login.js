c421904f175de3e30df96c489615ddbb45ce9904
Sourabh Patil <patilsourabh45@gmail.com>
2022-02-21 19:39:46 +0530

    Login & Signup Ui Added

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,51 @@
-import React from 'react'
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
+import "../css/login.scss"
+import TextField from '@mui/material/TextField';
+import {Link} from "react-router-dom"
 
-function Login(){
-    return(
-        <>
-        <h1>This is Login Page</h1>
-        </>
-    )
+const bull = (
+  <Box
+    component="span"
+    sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
+  >
+    •
+  </Box>
+);
+
+export default function Login() {
+  return (
+    <Grid className="loginCard" item xs={12} sm={6} lg={4}>  
+    <Card className="login-card-1" sx={{ minWidth: 275 }}>
+      <CardContent>
+
+        <Typography style={{textAlign:"center"}} sx={{ fontSize: 22 }} color="text.secondary" gutterBottom>
+         Login
+        </Typography>
+
+        <div className="email-margin">
+        <TextField className="email-text" id="standard-basic" label="Email" variant="standard" /> 
+        </div>
+
+        <div  className="email-margin">
+        <TextField className="email-text"  id="standard-basic" label="Password" variant="standard" /> 
+        </div>   
+      </CardContent>
+
+      <Typography className="login-policy" color="text.secondary">By continuing, you agree to Flipkart's <Link className="login-policy-1" to="#">Terms of Use </Link>and <Link className="login-policy-1"  to="#">Privacy Policy</Link>.</Typography>
+      
+      <CardActions style={{justifyContent:"center"}}>
+        <Button size="large"  variant="contained">Login</Button>
+      </CardActions>
+
+      <p className="p-style">Don't have an account? <Link className="card-link" to="/signup">Signup</Link></p>
+    </Card>
+    </Grid>
+  );
 }
-
-export default Login;
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,11 +1,53 @@
-import React from 'react'
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
+import "../css/login.scss"
+import TextField from '@mui/material/TextField';
+import {Link} from "react-router-dom"
 
-function Signup(){
-    return(
-        <>
-        <h1>This is Signup Page</h1>
-        </>
-    )
+const bull = (
+  <Box
+    component="span"
+    sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
+  >
+    •
+  </Box>
+);
+
+export default function Signup() {
+  return (
+    <Grid className="signupCard" item xs={12} sm={6} lg={4}>  
+    <Card className="signup-card" sx={{ minWidth: 275 }}>
+      <CardContent>
+        <Typography style={{textAlign:"center"}} sx={{ fontSize: 22 }} color="text.secondary" gutterBottom>
+         Signup
+        </Typography>
+        <div className="email-margin">
+        <TextField className="email-text"  id="standard-basic" label="Name" variant="standard" /> 
+        </div>  
+        <div className="email-margin">
+        <TextField className="email-text" id="standard-basic" label="Email" variant="standard" /> 
+        </div>
+        <div  className="email-margin">
+        <TextField className="email-text"  id="standard-basic" label="Mobile Number" variant="standard" /> 
+        </div>
+        <div  className="email-margin">
+        <TextField className="email-text"  id="standard-basic" label="New Password" variant="standard" /> 
+        </div>  
+        <div>
+        <TextField className="email-text"  id="standard-basic" label="Confirm Password" variant="standard" /> 
+        </div>     
+      </CardContent>
+      <CardActions style={{justifyContent:"center"}}>
+        <Button size="large" variant="contained">Signup</Button>
+      </CardActions>
+      <p className="p-style">Already have an account? <Link className="card-link" to="/login">Login</Link></p>
+    </Card>
+    </Grid>
+  );
 }
-
-export default Signup;
