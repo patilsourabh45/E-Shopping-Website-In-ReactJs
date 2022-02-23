2a2c18f1758a538a56848bab3c8636661d30cfac
Sourabh Patil <patilsourabh45@gmail.com>
2022-02-23 13:34:57 +0530

    Login design changed

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,67 +1,139 @@
 import * as React from 'react';
-import Box from '@mui/material/Box';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
+import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import Grid from '@mui/material/Grid';
-import "../css/login.scss"
+import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
-import {Link} from "react-router-dom"
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+// import Link from '@mui/material/Link';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import '../css/login.scss'
+import image from "../images/image2.jpg"
+import {Link} from 'react-router-dom'
 
-const bull = (
-  <Box
-    component="span"
-    sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
-  >
-    •
-  </Box>
-);
+function Copyright(props) {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Copyright © '}
+      <Link color="inherit" to="#">
+        Flipkart
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+const theme = createTheme();
 
 export default function Login() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    // eslint-disable-next-line no-console
+    console.log({
+      email: data.get('email'),
+      password: data.get('password'),
+    });
+  };
+
   return (
-    <div className="login-background">
-    <Grid className="loginCard " item xs={12} sm={6} lg={4}>  
-    <Card className="login-card-1" sx={{ minWidth: 275 }}>
-      <CardContent>
-
-        <Typography className="login-heading" color="text.secondary" gutterBottom>
-         Login
-        </Typography>
-
-        <div className="email-margin">
-        <TextField  
-        required 
-        placeholder="Enter Email"
-        className="email-text" 
-        id="standard-basic" 
-        label="Email" 
-        variant="standard" /> 
-        </div>
-
-        <div className="email-margin">
-        <TextField 
-        required 
-        placeholder="Enter Password"
-        className="email-text"  
-        id="standard-basic" 
-        label="Password" 
-        variant="standard" /> 
-        </div>   
-      </CardContent>
-
-      <Typography className="forgot-password"><Link className="card-link" to="#">Forgot Password ?</Link></Typography>
-
-      <Typography className="login-policy" color="text.secondary">By continuing, you agree to Flipkart's <Link className="login-policy-1" to="#">Terms of Use </Link>and <Link className="login-policy-1"  to="#">Privacy Policy</Link>.</Typography>
-      
-      <CardActions style={{justifyContent:"center"}}>
-        <Button size="large"  variant="contained">Login</Button>
-      </CardActions>
-
-      <p className="p-style">Don't have an account? <Link className="card-link" to="/signup">Signup</Link></p>
-    </Card>
-    </Grid>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Grid container component="main" sx={{ height: '100vh' }}>
+        <CssBaseline />
+        <Grid
+        className="image-1"
+          item
+          xs={false}
+          sm={4}
+          md={7}
+          sx={{
+            // backgroundImage: "../images/image2.jpg",
+            backgroundRepeat: 'no-repeat',
+            backgroundColor: (t) =>
+              t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+          }}
+        >
+          {/* <img className="background-img" alt="background-image" src={image} /> */}
+          
+          </Grid>
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+          <Box
+            sx={{
+              my: 8,
+              mx: 4,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          >
+            <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Login
+            </Typography>
+            <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
+              <TextField
+              placeholder="Enter Email"
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
+                className="email-text" 
+              />
+              <TextField
+              placeholder="Enter Password"
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+                className="email-text" 
+              />
+              <FormControlLabel
+                control={<Checkbox value="remember" color="primary" />}
+                label="Remember me"
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Login
+              </Button>
+              <Grid container>
+                <Grid item xs>
+                  <Link className="card-link" to="#">
+                    Forgot password?
+                  </Link>
+                </Grid>
+                <Grid item >
+                  <Link className="card-link" to="/signup">
+                    Don't have an account? Sign Up
+                  </Link>
+                </Grid>
+              </Grid>
+              <Copyright style={{marginTop:"15px"}}/>
+            </Box>
+          </Box>
+        </Grid>
+      </Grid>
+    </ThemeProvider>
   );
 }
